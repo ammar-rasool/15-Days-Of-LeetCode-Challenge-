73a6b84ec70a4f734bdc999e93fd6d12d924f0b8
ammar-rasool <159182363+ammar-rasool@users.noreply.github.com>
2024-12-03 11:01:57 +0500

    Time: 4 ms (76.38%) | Memory: 44.7 MB (22.25%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -7,6 +7,8 @@ class Solution {
         char[] ch2 = t.toCharArray();
         Arrays.sort(ch1);
         Arrays.sort(ch2);
-        return Arrays.equals(ch1, ch2);
+        s = new String(ch1);
+        t = new String(ch2);
+        return s.equals(t);
     }
 }
