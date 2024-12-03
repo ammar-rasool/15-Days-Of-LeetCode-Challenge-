7c661b7c4f5713a61a0133d7a67d962473fab1a2
ammar-rasool <159182363+ammar-rasool@users.noreply.github.com>
2024-12-03 11:00:32 +0500

    Time: 3 ms (90.09%) | Memory: 45.1 MB (6.19%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,18 +1,14 @@
 import java.util.Arrays;
 class Solution {
     public boolean isAnagram(String s, String t) {
-           s = s.replace(" ", "");
-            t = t.replace(" ", "");
+        s = s.replace(" ", "");
+        t = t.replace(" ", "");
         char[] ch1 = s.toCharArray();
         char[] ch2 = t.toCharArray();
         Arrays.sort(ch1);
         Arrays.sort(ch2);
         s = new String(ch1);
         t = new String(ch2);
-        if (s.equals(t)){
-            return true;
-        }else{
-            return false;
-        }
+        return s.equals(t);
     }
 }
