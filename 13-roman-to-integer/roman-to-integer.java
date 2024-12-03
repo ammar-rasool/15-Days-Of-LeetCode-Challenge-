77e0c379147029a9e8dc12c1f58d49d69e0a25a7
ammar-rasool <159182363+ammar-rasool@users.noreply.github.com>
2024-12-03 12:21:26 +0500

    Time: 2 ms (100.00%) | Memory: 44.3 MB (96.65%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.java
@@ -0,0 +1,52 @@
+class Solution {
+    public int romanToInt(String s) {
+       char[] chr = s.toCharArray();
+       int total = 0;
+       for(int i = 0; i < chr.length; i++){
+            if (i + 1 < chr.length && chr[i] == 'I' && chr[i+1] == 'V'){
+                total += 4;
+                i ++;
+            }else if(i + 1 < chr.length && chr[i] == 'I' && chr[i + 1] == 'X'){
+                total += 9;
+                i++;
+            }else if(i + 1 < chr.length && chr[i] == 'X' && chr[i + 1] == 'L'){
+                total += 40;
+                i++;
+            }else if(i + 1 < chr.length && chr[i] == 'X' && chr[i + 1] == 'C'){
+                total += 90;
+                i++;
+            }else if(i + 1 < chr.length && chr[i] == 'C' && chr[i + 1] == 'D'){
+                total += 400;
+                i++;
+            }else if(i + 1 < chr.length && chr[i] == 'C' && chr[i + 1] == 'M'){
+                total += 900;
+                i++;
+            }else{
+                switch (chr[i]){
+                case 'I':
+                    total += 1;
+                    break;
+                case 'V':
+                    total += 5;
+                    break;
+                case 'X':
+                    total += 10;
+                    break;
+                case 'L':
+                    total += 50;
+                    break;
+                case 'C':
+                    total += 100;
+                    break;
+                case 'D':
+                    total += 500;
+                    break;
+                case 'M':
+                    total += 1000;
+                    break;
+                }
+            }
+       }
+       return total;
+    }
+}
