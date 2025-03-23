69faaa52e2404236d4c1c8e04d851eaaadde1764
ammar-rasool <159182363+ammar-rasool@users.noreply.github.com>
2025-03-23 19:44:08 +0500

    Time: 0 ms (100.00%) | Memory: 8.7 MB (6.19%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -27,10 +27,8 @@ public:
         if (x > INT_MAX || x <INT_MIN){
             return 0;
         }else{
-            int digit = 0;
             while (x != 0){
-                digit = x % 10;
-                num  = num * 10 + digit;
+                num  = num * 10 + (x % 10);
                 x /= 10;
             }
             if(num > INT_MAX || num <INT_MIN){
