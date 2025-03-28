d1f36861d64a18d358a3e38143f42567b62eeebe
ammar-rasool <159182363+ammar-rasool@users.noreply.github.com>
2025-03-28 20:40:51 +0500

    Time: 0 ms (100.00%) | Memory: 16.7 MB (31.40%) - LeetSync

---
+++ 61-rotate-list/rotate-list.cpp
@@ -0,0 +1,59 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(head == nullptr){
+            return head;
+        }
+        vector<int> data;
+        ListNode* temp = head;
+        while(temp != nullptr){
+            data.emplace_back(temp->val);
+            temp = temp->next;
+        }
+        if(data.size() == 1){
+            head = new ListNode(data[0]);
+            return head;
+        }
+        k = k % data.size();
+        int last = 0;
+        for (int i = 0; i < k; i++){
+            last = data[data.size() - 1];
+            for(int i = data.size() - 1; i >= 0; i--){
+                if (i - 1 >= 0){
+                    data[i] = data[i - 1];
+                }else{
+                    data[0] = last;
+                }
+            }
+        }
+        temp = new ListNode(0);
+        head = temp;
+        for (int i = 0; i < data.size(); i++){
+            temp->next = new ListNode(data[i]);
+            temp = temp->next;
+        }
+        return head->next;
+        
+
+        
+    }
+    // void rotate(vector<int> data, start, end){
+    //     for(int i = start, i < end, i++){
+    //         if (i + 1 < end){
+    //             int temp = data[i];
+    //             data[i] = data[i + 1];
+    //             data[i + 1] = temp;
+    //         }
+    //     }
+    // }
+};
