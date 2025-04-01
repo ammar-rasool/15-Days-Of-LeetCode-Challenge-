34db80e3a9a4b701bbfb07232a8fe0004e08553c
ammar-rasool <159182363+ammar-rasool@users.noreply.github.com>
2025-04-01 19:43:05 +0500

    Time: 0 ms (100.00%) | Memory: 8.4 MB (81.11%) - LeetSync

--- 3447-clear-digits/clear-digits.cpp
+++ 3447-clear-digits/clear-digits.cpp
@@ -4,15 +4,6 @@ public:
         if(s == "" || (s.length() == 1 && isalpha(s[0]))){
             return s;
         }else{
-            // stack<char>ch;
-            int size = s.length();
-            // for(int i = 0; i < size; i++){
-            //     if(isdigit(s[i])){
-            //         s = s.erase(i-1, i);
-            //         size = s.length();
-            //         i = 0;
-            //     }
-            // }
             int i = 1;
             while(i < s.length()){
                 if(isdigit(s[i])){
